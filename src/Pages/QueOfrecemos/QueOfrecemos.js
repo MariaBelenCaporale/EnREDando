7ca5e9cb5e30019fc48bce78f60fa9a960ffef75
Belen Caporale <caporalebelen14@gmail.com>
2023-07-04 15:50:00 -0300

    responsive ofrecemos

--- src/Pages/QueOfrecemos/QueOfrecemos.js
+++ src/Pages/QueOfrecemos/QueOfrecemos.js
@@ -9,102 +9,76 @@ import bannerTres from '../../components/Assets/ofrecemos/bannerTres.png';
 import capaDos from '../../components/Assets/ofrecemos/capaDos.png';
 import materialDos from '../../components/Assets/ofrecemos/materialDos.png';
 
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 const QueOfrecemos = () => {
     return (
-        <div>
+        
+        <Container>
 
-            <div className='containerOfrecemos'>
-                <div className='quienes'>
+            <Row className='quienes'>
+                <Col>
                     <p className='tituloQuienes'>¿QUÉ OFRECEMOS?</p>
-                </div>
+                </Col>
+            </Row>
 
-                <div className='containerCuadroOfrecemos'>
-                    <div className='containerB'>
-                    <div className='cuadroOfrece'>
-                    
-                    <div className='containerIcon'>
-                    <div className='iconoOfrecemos'>
-                        <img className='imgOfrecemos' src={hoja} alt='icono talleres' />
-                        </div>
-                    </div>
-
-                       
-
-
-                        <div className='contieneTodo'>
+            <Row className='parteCuadroOfre' xxl={12} xl={12} lg={12} md={12} >
+                <Col xl={3} lg={3} md={12} className='contenedorCuadrosOfre'>
+                    <Col className='iconoOfrecemos'>
+                    <Image fluid className='imgOfrecemos' src={hoja} alt='icono talleres' />
+                    </Col>
+                    <Col className='cuadrosOfre'>
                         <h1 className='tituloQuienesUno'>TALLERES</h1>
                         <p className='textoOfrecemos'>Ofrecemos talleres de diferentes temas dirigidos a adolescentes y jóvenes, brindando información y herramientas valiosas para mejorar la calidad de vida de las personas. Es una oportunidad para seguir aprendiendo.
                         </p>
-                        </div>
-                        <div className='containerButton'>
+                        <Col className='botonMasOf'>
                         <button className='botonOfrecemos'>
                         <a className='linkeado' href="#talleres">VER MÁS  </a>
-                           </button> 
-                        </div>
-                    </div>
+                        </button>
+                        </Col>
+                    </Col>
+                </Col>
 
-                    <div className='cuadroOfrece'>
-
-                    <div className='containerIcon'>
-                    <div className='iconoOfrecemos'>
-                        <img className='imgOfrecemos' src={parlante} alt='icono capacitaciones' />
-                        </div>
-                    </div>
-                       
-
-
-                        <div className='contieneTodo'>
+                <Col xl={3} lg={3} md={12} className='contenedorCuadrosOfre'>
+                    <Col className='iconoOfrecemos'>
+                    <Image fluid className='imgOfrecemos' src={parlante} alt='icono capacitaciones' />
+                    </Col>
+                
+                    <Col className='cuadrosOfre'>
                         <h1 className='tituloQuienesUno'>CAPACITACIONES</h1>
-                        <p className='textoOfrecemos'> ¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestra capacitación!  ¿Estás listo/a para impulsar tu carrera y adquirir conocimientos valiosos? ¡No busques más! 
+                        <p className='textoOfrecemos'>¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestra capacitación!  ¿Estás listo/a para impulsar tu carrera y adquirir conocimientos valiosos? ¡No busques más! 
                         </p>
-                        </div>
-
-                        <div className='containerButton'>
+                        <Col className='botonMasOf'>
                         <button className='botonOfrecemos'>
-                        <a className='linkeado' href="#capacitaciones">VER MÁS  </a></button> 
-                        </div>
-                        
-                    </div>
+                        <a className='linkeado' href="#talleres">VER MÁS  </a>
+                        </button>
+                        </Col>
+                    </Col>
+                </Col>
 
-
-                    <div className='cuadroOfrece'>
-                        
-
-                    <div className='containerIcon'>
-                    <div className='iconoOfrecemos'>
-                        <img className='imgOfrecemos' src={carpeta} alt='icono material educativo'/>
-                        </div>
-                    </div>
-                        <div className='contieneTodo'>
+                <Col xl={3} lg={3} md={12} className='contenedorCuadrosOfre'>
+                <Col className='iconoOfrecemos'>
+                <Image fluid className='imgOfrecemos' src={carpeta} alt='icono material educativo'/>
+                </Col>
+                    <Col className='cuadrosOfre'>
                         <h1 className='tituloQuienesUno'>MATERIAL EDUCATIVO</h1>
                         <p className='textoOfrecemos'>Te brindamos mayor información y compartidos por este medio material que puede ayudarte a profundizar temas aprendidos en talleres y capacitaciones. Además, de información importante para reforzar tus conocimientos.
                         </p>
-                        </div>
-                        <div className='containerButton'>
+                        <Col className='botonMasOf'>
                         <button className='botonOfrecemos'>
-                        <a className='linkeado' href="#material">VER MÁS  </a>
-                        </button> 
-                        </div>  
-                    </div>       
-          
-                    </div>
-                </div>
+                        <a className='linkeado' href="#talleres">VER MÁS  </a>
+                        </button>
+                        </Col>
+                    </Col>
+                </Col>
+            </Row>
 
 
 
-                <div className='sectionTalleres'>
-                
-                <div className='talleres' id='talleres'>
-                    <h2>TALLERES</h2>
-                    <div className='textosTalleres'>
-                    <p> ¡Alcanza tu máximo potencial con nuestra capacitación! Beneficios de nuestros talleres.
-
-
-
-
-
-</p>
+            <Row className='parteOfre Ofre' id="talleres">
+            <Col xxl={7} xl={7} lg={5} md={12} sm={12} >
+                <h2>TALLERES</h2>
+                <p> ¡Alcanza tu máximo potencial con nuestra capacitación! Beneficios de nuestros talleres.</p>
                     <p>
                     1- Oportunidad única para aprender de diferentes temas de salud, educación sexual, entre otros temas.
                     </p>
@@ -117,36 +91,24 @@ const QueOfrecemos = () => {
                     <p>
                     El objetivo de los talleres es tener una conversación y obtener opiniones, preocupaciones, posibles barreras y necesidades que tienen las personas adolescentes y jóvenes de toda la provincia de Santa Fe 
                     </p>
-                    </div>
-                </div>
-
-                <div className='fotosOfrecemos'>
-
-                <img className='imgOfrecemos' src={chicosDos} alt='chicos en clase'/>
-                </div> 
-            </div>
+            </Col>
+            <Col className='imagenOfreTaller' xxl={4} xl={4} lg={5} md={6} sm={6}>
+                <Image fluid  src={chicosDos} />
+            </Col>
+            </Row>
 
 
-            <div className='bannerOfrecemos'>
-                <img className='imgOfrecemos' src={bannerUno} alt='Banner desarrolla tus habilidades'/>
-            </div>
-            
-
-
-            <div className='sectionTalleres'>
-                
-                <div className='talleres'>
-                    <h2 id='capacitaciones'>CAPACITACIONES</h2>
-                    <div className='textosTalleres'>
-                    <p>¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestras capacitaciones!. Beneficios de capacitarte con nosotros.
- 
+        <Row>
+        <Image fluid src={bannerUno} alt='Banner desarrolla tus habilidades'/>
+        </Row>
 
 
 
-                    </p>
-                    <p>
-                    1- 
-Amplía tus conocimientos: Nuestras capacitaciones están diseñadas por expertos en el campo, lo que te brinda acceso a información actualizada y relevante.
+        <Row className='parteOfre Ofre' id="talleres">
+            <Col xxl={7} xl={7} lg={5} md={12} sm={12} >
+                <h2>CAPACITACIONES</h2>
+                <p>¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestras capacitaciones!. Beneficios de capacitarte con nosotros.</p>
+                    <p>1- Amplía tus conocimientos: Nuestras capacitaciones están diseñadas por expertos en el campo, lo que te brinda acceso a información actualizada y relevante.
                     </p>
                     <p>
                     2- Desarrollo profesional: La capacitación es una inversión. Nuestros programas están diseñados para abordar temas claves. 
@@ -157,43 +119,24 @@ Amplía tus conocimientos: Nuestras capacitaciones están diseñadas por experto
                     <p>
                     4- Aprendizaje práctico: Nuestras capacitaciones se enfocan en la aplicación práctica de los conocimientos.
                     </p>
-                    </div>
-                </div>
-
-                <div className='fotosOfrecemos'>
-
-                <img className='imgOfrecemos' src={capaDos} alt='chicos en clase'/>
-                </div> 
-            </div>
-
-
-            <div className='bannerOfrecemos'>
-                <img className='imgOfrecemos' src={bannerDos} alt='Banner desarrolla tus habilidades'/>
-            </div>
+            </Col>
+            <Col className='imagenOfreTaller' xxl={4} xl={4} lg={5} md={6} sm={6}>
+            <Image fluid src={capaDos} alt='chicos en clase'/>
+            </Col>
+            </Row>
             
 
+            <Row>
+            <Image src={bannerDos} alt='Banner desarrolla tus habilidades'/>
+            </Row>
 
 
-
-
-
-            <div className='sectionTalleres'>
-                
-                <div className='talleres'>
-                    <h2 id='material'>MATERIAL EDUCATIVO</h2>
-                    <div className='textosTalleres'>
-                    <p>¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestro material educativo. 
-
-
-
-
-
-
- 
-
-
-
-                    </p>
+           
+            
+            <Row className='parteOfre Ofre' id="talleres"  >
+            <Col xxl={7} xl={7} lg={5} md={12} sm={12} >
+                <h2>MATERIAL EDUCATIVO</h2>
+                    <p>¡Desarrolla tus habilidades y alcanza tu máximo potencial con nuestro material educativo. </p>
                     <p>
                     1- Material educativo con información actualizada y relevante. 
                     </p>
@@ -209,31 +152,17 @@ Amplía tus conocimientos: Nuestras capacitaciones están diseñadas por experto
                     <p>
                     5- Flexibilidad: Ofrecemos material educativo en línea o física de acuerdo a tu  tu disponibilidad y preferencia.
                     </p>
-                   
-                    </div>
-                </div>
+            </Col>
+            <Col className='imagenOfreTaller' xxl={4} xl={4} lg={5} md={6} sm={6}>
+            <Image fluid  src={materialDos} alt='chicos en clase'/>
+            </Col>
+            </Row>
 
-                <div className='fotosOfrecemos'>
+            <Row className='bannerTres'>
+            <Image src={bannerTres} alt='Banner desarrolla tus habilidades'/>
+            </Row>
 
-                <img className='imgOfrecemos' src={materialDos} alt='chicos en clase'/>
-                </div> 
-            </div>
-
-
-            <div className='bannerOfrecemos'>
-                <img className='imgOfrecemos' src={bannerTres} alt='Banner desarrolla tus habilidades'/>
-            </div>
-
-
-
-
-            
-
-
-
-
-            </div> 
-        </div>
+            </Container>
     );
 };
 
